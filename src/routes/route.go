dd6e88f582e42b4752052df4a78434ccf1548f45
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-10 21:49:26 +0800

    finish

--- src/routes/route.go
+++ src/routes/route.go
@@ -11,6 +11,8 @@ func SetupRoutes() *gin.Engine {
 	r.GET("/v1/mahasiswa", mahasiswarepositories.GetAllData)
 	r.POST("/v1/mahasiswa/create", mahasiswarepositories.CreateData)
 	r.GET("/v1/mahasiswa/get/:id", mahasiswarepositories.GetDataById)
+	r.POST("/v1/mahasiswa/update/:id", mahasiswarepositories.UpdateData)
+	r.DELETE("/v1/mahasiswa/delete/:id", mahasiswarepositories.DeleteData)
 
 	return r
 }
